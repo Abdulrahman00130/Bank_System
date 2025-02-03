3e5b885605b65b08b360ada8698be26e028b7bb8
Abdulrahman00130 <am9880375@gmail.com>
2025-02-03 02:50:14 +0200

    Switching to CodeBlocks

---
+++ Code/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+	Person p1("Ahmed", "264dgdsf6", "Birth Place?", "Cairo", 1, 32), p2;
+
+	p1.display();
+	p2.display();
+}
