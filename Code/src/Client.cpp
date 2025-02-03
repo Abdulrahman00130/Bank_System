90481d0f7e9d3d5f8664f7fce7c375290859ea28
Sehso11 <hussienmohammed154@gmail.com>
2025-02-03 17:14:40 +0200

    updates on client class

--- Code/src/Client.cpp
+++ Code/src/Client.cpp
@@ -1,11 +1,37 @@
+#include<iostream>
 #include "Client.h"
 
-Client::Client()
-{
-    //ctor
-}
 
-Client::~Client()
-{
-    //dtor
-}
+    Client::Client(){
+        balance = 0;
+    }
+    Client::Client(string name, string pass, string ques, string ans, int id, int age, double balance) : Person(name, pass, ques, ans, id, age){
+        this->balance = balance;
+    }
+    void Client::setBalance(double balance){
+        this->balance = balance;
+    }
+    double Client::getBalance(){
+        return balance;
+    }
+    void Client::deposit(double amount){
+        balance += amount;
+    }
+    void Client::withdraw(double amount){
+        balance -= amount;
+    }
+    void Client::transferTo(double amount, Client& recipient){
+        balance -= amount;
+        recipient.balance += amount;
+    }
+    void Client::checkBalance(){
+        cout << "Balance: " << balance << endl;
+    }
+    void Client::display(){
+        cout << "Name: " << name << endl;
+        cout << "Id: " << id << endl;
+        cout << "Age: " << age << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "===============================\n";
+    }
+
