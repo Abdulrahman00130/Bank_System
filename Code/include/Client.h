4ff4e494a62a84501c07dd13e7149f8f5ff1ff54
Sehso11 <hussienmohammed154@gmail.com>
2025-02-03 17:14:40 +0200

    updates on client class

--- Code/include/Client.h
+++ Code/include/Client.h
@@ -2,33 +2,18 @@
 #define CLIENT_H
 #include "Person.h"
 
-
-class Client : public Person
-{
+class Client : public Person{
     double balance;
 public:
     Client();
     Client(string name, string pass, string ques, string ans, int id, int age, double balance);
-    void setBalance(double balance){
-        this->balance = balance;
-    }
-    double getBalance(){
-        return balance;
-    }
-    void deposit(double amount){
-        balance += amount;
-    }
-    void withdraw(double amount){
-        balance -= amount;
-    }
-    void transferTo(double amount, Client& recipient){
-        balance -= amount;
-        recipient.balance += amount;
-    }
-    void checkBalance(){
-        cout << " Balance: " << balance;
-    }
-    void displayFunction
+    void setBalance(double balance);
+    double getBalance();
+    void deposit(double amount);
+    void withdraw(double amount);
+    void transferTo(double amount, Client& recipient);
+    void checkBalance();
+    void display();
 };
 
 #endif // CLIENT_H
--- Code/main.cpp
+++ Code/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Person.h"
+#include "Client.h"
 
 using namespace std;
 
@@ -28,4 +29,14 @@ int main()
 
 	p2.setPass("2s64dg56sdg1");
 	p2.display();
+
+	Client c1("Sehs", "9000pm2", "which school you learned in?", "Mogamaa", 555, 17, 10000.5);
+	c1.display();
+
+	c1.withdraw(5000);
+	c1.checkBalance();
+	c1.setBalance(600000);
+	c1.display();
+	cout << c1.getBalance();
+	c1.deposit(25000);
 }
