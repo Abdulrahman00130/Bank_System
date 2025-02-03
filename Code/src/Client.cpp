447785d2c49d923d354f16f47e08bee36ec9b54d
Sehso11 <hussienmohammed154@gmail.com>
2025-02-03 19:01:58 +0200

    updates

--- Code/src/Client.cpp
+++ Code/src/Client.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include "Client.h"
+#include "Validation.h"
 
 
     Client::Client(){
         balance = 0;
     }
+
+
+    ////////////////////////////
     Client::Client(string name, string pass, string ques, string ans, int id, int age, double balance) : Person(name, pass, ques, ans, id, age){
+        Validation::checkBal
         this->balance = balance;
     }
+    ///////////////////////////////
     void Client::setBalance(double balance){
         this->balance = balance;
     }
@@ -17,13 +23,16 @@
     void Client::deposit(double amount){
         balance += amount;
     }
+    //////////////////////////////////////////////////////////////////////////
     void Client::withdraw(double amount){
         balance -= amount;
     }
+    ///////////////////////////////////////////////////////////////////////////
     void Client::transferTo(double amount, Client& recipient){
         balance -= amount;
         recipient.balance += amount;
     }
+
     void Client::checkBalance(){
         cout << "Balance: " << balance << endl;
     }
