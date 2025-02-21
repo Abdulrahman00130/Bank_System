95e48aab8231397edd0f8177345eb568019469cd
hussein701 <ahussien4050@gmail.com>
2025-02-21 18:46:25 +0200

    phase 2 employee update

--- Code/include/Employee.h
+++ Code/include/Employee.h
@@ -36,11 +36,75 @@ protected:
             clientVect.push_back(client);
             clientLastID= client.getID();
         }
-        Client* searchId(int id)
+        int searchClient(int clID)
         {
           // binary search (id sorted)
+
+          int start{};
+          int ends= clientVect.size()-1;
+          int mid ;
+          while(true)
+          {
+              mid=(ends-start)/2;
+              if(clientVect[mid].getID()==clID)
+              return mid;
+
+              if(clientVect[mid].getID()>clID)
+              {
+                  ends=mid-1;
+              }
+              else
+              {
+                  start=mid+1;
+              }
+              if(ends<start) return -1;
+
+          }
           //add remove one client function to employee
         }
+
+        void listClient()
+        {
+            cout << "All Clients:\n";
+    for (int i = 0; i < clientVect.size(); i++)
+    {
+        clientVect[i].display();
+    }
+
+        }
+
+     void editClientName(string clName ,int index)
+     {
+         clientVect[index].setName(clName);
+     }
+
+      void editClientAge(int clAge ,int index)
+     {
+         clientVect[index].setAge(clAge);
+     }
+
+      void editClientPassword(string clPass ,int index)
+     {
+         clientVect[index].setPass(clPass);
+     }
+
+      void editClientBalance(double clbal ,int index)
+     {
+         clientVect[index].setBalance(clbal);
+     }
+
+//     void editClient(int index, int choice)
+//     {
+//        switch(choice)
+//        {
+//      case 1:
+//          cout<<"enter name\n";
+//        editClientName()
+//        }
+//
+//
+//     }
+
 
         void display ()
         {
